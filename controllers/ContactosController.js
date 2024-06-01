4a65f1dff659678b3819a0d622c07cfb00e6d8da
Aethz16 <sanchezldv1608@gmail.com>
2024-06-01 09:43:04 -0400

    funcionamiento

--- controllers/ContactosController.js
+++ controllers/ContactosController.js
@@ -6,8 +6,31 @@ class ContactosController {
     this.add = this.add.bind(this);
   }
 
+  async obtenerIp() {
+    try {
+      const response = await fetch('https://api.ipify.org?format=json');
+      const data = await response.json();
+      return data.ip; 
+    } catch (error) {
+      console.error('Error al obtener la ip:', error);
+      return null; 
+    }
+  }
+
+  async obtenerPais(ip) {
+    try {
+      const response = await fetch('https://ipinfo.io/'+ip+'?token=3cc1701489aa3e');
+      const data = await response.json();
+      return data.country; 
+    } catch (error) {
+      console.error('Error al obtener el país:', error);
+      return null; 
+    }
+  }
+
   async add(req, res) {
     // Validar los datos del formulario
+
 
     const { email, name, mensaje } = req.body;
 
@@ -17,10 +40,11 @@ class ContactosController {
     }
 
     // Guardar los datos del formulario
-    const ip = req.ip;
+    const ip = await this.obtenerIp();
     const fecha = new Date().toISOString();
+    const pais = await this.obtenerPais(ip);
     
-    await this.contactosModel.crearContacto(email, name, mensaje, ip, fecha);
+    await this.contactosModel.crearContacto(email, name, mensaje, ip, fecha, pais);
 
     const contactos = await this.contactosModel.obtenerAllContactos();
 
--- public/javascripts/ReCaptcha.js
+++
@@ -1,8 +0,0 @@
-function verificarCaptcha(event) {
-    let response = grecaptcha.getResponse();
-    if (response.length === 0) {
-        console.log("Por favor, complete correctamente el captcha.");
-        event.preventDefault();
-    }
-}
-document.getElementById("formulario").addEventListener("submit", verificarCaptcha);
