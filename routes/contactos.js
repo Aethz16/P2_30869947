4436262e9acb5c6cfa0b73850524e467d9390516
Aethz16 <sanchezldv1608@gmail.com>
2024-06-21 21:02:46 -0400

    view_error

--- routes/contactos.js
+++ routes/contactos.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const conexion = require('../conf/test.db')
+var express = require('express');
+var router = express.Router();
+var db = require('../conf/database');
 
-router.get('/',(req,res)=>{
-  conexion.query('SELECT * FROM contactos',(error,results)=>{
-      if(error){
-          throw error;
-      }else {
-          res.render('index',{results:results});
-  }
-  })
+/* GET contactos page. */
+router.get('/', function(req, res, next) {
+  const query = 'SELECT * FROM contactos'; 
+
+  db.all(query, [], (err, rows) => {
+    console.log(rows)
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.render('contactos', { data: rows });
   });
+});
 
-  module.exports = router
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const contactosController = new ContactosController();
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: express});
 });
