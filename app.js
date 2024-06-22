8036f1013a58730046f8706035982755b89a36c6
Aethz16 <sanchezldv1608@gmail.com>
2024-06-22 15:48:59 -0400

    verificando

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+var LocalStrategy = require('passport-local').Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+var db = require('./conf/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var contactosRouter = require('./routes/contactos');
+var authRouter = require('./routes/auth'); 
 
 var app = express();
 
@@ -19,8 +29,67 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(session({
+  secret: process.env.SECRET, // Cambia esto por una clave secreta
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(flash());
+
+// Middleware para hacer mensajes flash disponibles en todas las vistas
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const predefinedUser = process.env.USUARIO;
+const predefinedPass = process.env.PASSWORD;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    if (username === predefinedUser && password === predefinedPass) {
+      return done(null, { username: predefinedUser });
+    } else {
+      return done(null, false, { message: 'Usuario o contraseña incorrectos' });
+    }
+  }
+));
+
+// Configura el middleware para analizar el cuerpo de las peticiones
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contactos', contactosRouter);
+app.use('/auth', authRouter);
+
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "https://p2-30869947.onrender.com/auth/google/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/contactos.js
+++ routes/contactos.js
@@ -1,17 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var db = require('../conf/database');
+var db =require("../conf/database");
 
-/* GET contactos page. */
-router.get('/', function(req, res, next) {
-  const query = 'SELECT * FROM contactos'; 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { // Esto asume que estás usando algún método de autenticación que añade esta función al request
+    return next();
+  }
+  res.redirect('/auth/login'); // Si no está autenticado, redirige al login
+}
+
+router.get('/', ensureAuthenticated, (req, res, next) => {
+  const query = "SELECT * FROM contactos"; 
 
   db.all(query, [], (err, rows) => {
     console.log(rows)
     if (err) {
       return res.status(500).send(err.message);
     }
-    res.render('contactos', { data: rows });
+    res.render("contactos", {title: "Contactos", data: rows });
   });
 });
 
